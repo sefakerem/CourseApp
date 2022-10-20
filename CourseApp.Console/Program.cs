92c5dd8e69b146f64dbfc8bd94c694011e6bc362
Sefa Kerem Koçakoğlu <sefakeremkocakoglu@Sefa-MacBook-Pro.local>
2022-10-20 18:52:56 +0300

    Operatörler

---
+++ CourseApp.Console/Program.cs
@@ -0,0 +1,52 @@
+﻿
+#region Operatörler
+
+/*
+ var cevap = 4!=4;
+Console.WriteLine(cevap);
+ Aritmetik Operatörler:
+ Ekleme + 2+2
+ Çıkarma - 5-3
+ Bölme /
+ Mod Alma %
+ Çarpma *
+
+ Tekli Operatörler:
+ Arttırma ve Azaltma Operatörü (++ / --)
+ Değil Operatörü (!)
+ Atama Operatörleri:
+ = Operatörü
+ += Operatörü
+ -= Operatörü
+ *= Operatörü
+ /= Operatörü
+
+ İlişkisel Operatör:
+ Küçüktür <
+ Büyüktür >
+ Küçük Eşittir <=
+ Büyük Eşittir >=
+ Eşittir ==
+ Eşit Değildir !=
+
+ Mantıksal Operatörler:
+ & (ve) Mantıksal Operatörü
+ | (veya) Mantıksal Operatörü
+ && (koşullu ve) Mantıksal Operatörü
+ || (koşullu veya) Mantıksal Operatörü
+ ! (değil) Mantıksal Operatörü
+*/
+
+
+#endregion
+
+#region Degiskenler
+//bool dogruMu = true;
+//Console.WriteLine(dogruMu);
+//Integer:tamsayı int sayi = 12;
+//String:metin string metin = "NASILSIN";
+//Double:ondalikliSayi double sayi = 12.3;
+//Char:karekter char harf = 'A';
+//Boolean:true veya false
+#endregion
+
